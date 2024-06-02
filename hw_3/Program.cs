c5f88ea34f60ad281bed5c430f68afec9094f528
Renat <renat@MacBook-Air-Renat.local>
2024-06-02 21:05:08 +0300

    Решение задачи 3

--- hw_3/Program.cs
+++ hw_3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 3: Задайте произвольный массив.
+//  Выведете его элементы, начиная с конца. 
+//  Использовать рекурсию, не использовать циклы.
+//  [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+void PrintReversArray(int[] numbers, int i = 0)
+{
+    if (i == numbers.Length)
+    {
+        return;
+    }
+    PrintReversArray(numbers, i + 1);
+    Console.Write($"{numbers[i]} ");
+}
+
+int[] GetArray()
+{
+    int[] array = [1, 2, 5, 0, 10, 34];
+    return array;
+}
+
+int[] arr = GetArray();
+PrintReversArray(arr);
